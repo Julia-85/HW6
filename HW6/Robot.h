081c03642fd85f1efceb2ebe6a72c3219d8ffc04
Julia-85 <120277347+Julia-85@users.noreply.github.com>
2024-03-19 01:01:15 +0300

    Create Robot.h

---
+++ HW6/Robot.h
@@ -0,0 +1,10 @@
+
+#import <Foundation/Foundation.h>
+
+@interface Robot : NSObject
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, strong) NSString *name;
+
+@end
