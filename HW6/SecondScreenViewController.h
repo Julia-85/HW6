cf02e78add44d2606f00eb66eaf920cb10ce07fd
Julia-85 <120277347+Julia-85@users.noreply.github.com>
2024-03-19 01:02:34 +0300

    Create SecondScreenViewController.h

---
+++ HW6/SecondScreenViewController.h
@@ -0,0 +1,11 @@
+
+#ifndef SecondScreenViewController_h
+#define SecondScreenViewController_h
+
+
+#endif /* SecondScreenViewController_h */
+#import <UIKit/UIKit.h>
+
+@interface SecondScreenViewController : UIViewController
+
+@end
