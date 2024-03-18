a77bfc7e550ca3fbeec01c4a0303a4e53aadbcea
Julia-85 <120277347+Julia-85@users.noreply.github.com>
2024-03-19 00:57:08 +0300

    Create FirstScreenViewController.h

---
+++ HW6/FirstScreenViewController.h
@@ -0,0 +1,12 @@
+
+
+#ifndef FirstScreenViewController_h
+#define FirstScreenViewController_h
+
+
+#endif /* FirstScreenViewController_h */
+#import <UIKit/UIKit.h>
+
+@interface FirstScreenViewController : UIViewController
+
+@end
